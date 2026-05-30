17e192eca4b0baa1e6270818be56f1edbc06f6bd
Zaa <zaa@example.com>
2026-05-30 23:30:57 +0200

    overlay: rate-vakjes hoger (sluit tussenruimte boven de rij)
    containerTop van chartTop+28 naar chartTop-12 zodat de rij rate-vakjes
    omhoog schuift, tegen het bovenblok (klok/hypokaart/waarde) aan.

--- nightscout-overlay/rate-overlay.js
+++ nightscout-overlay/rate-overlay.js
@@ -1126,7 +1126,7 @@
     var statusLine = document.querySelector('.currentDetails, .currentStatus, #currentDetails');
     var buttonHeight = button.getBoundingClientRect().height || 24;
     var buttonTop = chartTop - buttonHeight - 6;
-    var containerTop = chartTop + 28;
+    var containerTop = chartTop - 12;
     var alertTop = chartTop + 4;
     var nav = ensureHistoryNav();
     if (bgValue && clock) {
